3ea40e8cdf36a73959c3d6796fff53d70aeb306a
Vi39 <45115080+Vi39@users.noreply.github.com>
2018-12-02 02:09:38 +0530

    Create sum.py

---
+++ sum.py
@@ -0,0 +1,9 @@
+import sys
+def main(sys.argv[sys.in]):
+        a=sys.argv[1]
+        b=sys.argv[2]
+        sum=sys.argv[1]+sys.argv[2]
+        print('Sum%s' %sum)
+    
+__name__=='__main__':
+    main()
